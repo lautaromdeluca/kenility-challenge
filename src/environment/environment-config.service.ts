f676291daa4ce349a060e96ec28346040588a78e
Lautaro De Luca <ldeluca@arcor.com>
2025-03-28 16:12:25 +0100

    feat(env-guard-check): added guard checks on environment variables

--- src/environment/environment-config.service.ts
+++ src/environment/environment-config.service.ts
@@ -3,20 +3,39 @@ import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class EnvironmentConfigService {
-  constructor(private readonly configService: ConfigService) { }
-  getMongoUser(): string {
-    return this.configService.getOrThrow<string>('MONGO_USER')
+  constructor(private readonly configService: ConfigService) {}
+  getMongoUser() {
+    const mongoUser = this.configService.getOrThrow<string>('MONGO_USER')
+    if (typeof mongoUser !== 'string')
+      throw new Error('MONGO_USER is not a string')
+    return mongoUser
   }
 
-  getMongoPassword(): string {
-    return this.configService.getOrThrow<string>('MONGO_PASSWORD')
+  getMongoPassword() {
+    const mongoPassword =
+      this.configService.getOrThrow<string>('MONGO_PASSWORD')
+    if (typeof mongoPassword !== 'string')
+      throw new Error('MONGO_PASSWORD is not a string')
+    return mongoPassword
   }
 
-  getMongoURL(): string {
-    return this.configService.getOrThrow<string>('MONGO_URL')
+  getMongoURL() {
+    const mongoURL = this.configService.getOrThrow<string>('MONGO_URL')
+    if (typeof mongoURL !== 'string')
+      throw new Error('MONGO_URL is not a string')
+    return mongoURL
   }
 
-  getSecret(): string {
-    return this.configService.getOrThrow<string>('JWT_SECRET')
+  getSecret() {
+    const jwtSecret = this.configService.getOrThrow<string>('JWT_SECRET')
+    if (typeof jwtSecret !== 'string')
+      throw new Error('JWT_SECRET is not a string')
+    return jwtSecret
+  }
+
+  getPort(): number {
+    const port = Number(this.configService.getOrThrow<string>('PORT'))
+    if (typeof port !== 'number') throw new Error('PORT is not a number')
+    return port
   }
 }
